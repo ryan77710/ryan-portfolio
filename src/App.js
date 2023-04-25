0962812ef5494ae3aac8db81bb34aed438f0bfc5
ryan77710 <lolliaryan@ryan-ordi.local>
2023-04-25 08:09:52 +0200

    update 2022

--- src/App.js
+++ src/App.js
@@ -11,12 +11,7 @@ import PictureProject from "./Components/PictureProject";
 
 //import icon
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faStar,
-  faStarHalfAlt,
-  faMoon,
-  faSun,
-} from "@fortawesome/free-solid-svg-icons";
+import { faStar, faStarHalfAlt, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Icon } from "@iconify/react";
 import reactIcon from "@iconify-icons/logos/react";
@@ -50,218 +45,81 @@ function App() {
 
   return (
     <div className="body">
-      <button
-        onClick={() => setToggle((x) => !x)}
-        className={`toggle-theme ${toggle ? "" : ""}`}
-      >
-        <FontAwesomeIcon
-          className="toggle-icon"
-          icon={!toggle ? "sun" : "moon"}
-          color={"grey"}
-        />
+      <button onClick={() => setToggle((x) => !x)} className={`toggle-theme ${toggle ? "" : ""}`}>
+        <FontAwesomeIcon className="toggle-icon" icon={!toggle ? "sun" : "moon"} color={"grey"} />
       </button>
 
-      <div
-        data-scroll-container
-        data-scroll-section
-        className={`App ${toggle ? "App-light" : "App-dark"}`}
-      >
-        <div
-          id="pin"
-          className={`presentation ${
-            toggle ? "presentation-light" : "presentation-dark"
-          }`}
-        >
+      <div data-scroll-container data-scroll-section className={`App ${toggle ? "App-light" : "App-dark"}`}>
+        <div id="pin" className={`presentation ${toggle ? "presentation-light" : "presentation-dark"}`}>
           <div className="presentation-container-left">
-            <button
-              onClick={() => setScrollAnimation((x) => !x)}
-              className="animation-button"
-            >
+            <button onClick={() => setScrollAnimation((x) => !x)} className="animation-button">
               scroll animation : {scrollAnimation ? "on" : "off"}
             </button>
 
-            <h1
-              data-scroll
-              data-scroll-speed="3"
-              onMouseEnter={() => setToggleHeader(true)}
-            >
+            <h1 data-scroll data-scroll-speed="3" onMouseEnter={() => setToggleHeader(true)}>
               LOLLIA Ryan
             </h1>
-            <h2
-              data-scroll
-              data-scroll-speed="-2"
-              onMouseEnter={() => setToggleHeader((x) => !x)}
-            >
+            <h2 data-scroll data-scroll-speed="-2" onMouseEnter={() => setToggleHeader((x) => !x)}>
               Développeur junior front-end
             </h2>
-            <h3
-              data-scroll
-              data-scroll-speed="-2"
-              onMouseEnter={() => setToggleHeader(false)}
-            >
+            {/* <h3 data-scroll data-scroll-speed="-2" onMouseEnter={() => setToggleHeader(false)}>
               En recherche de stage
-            </h3>
+            </h3> */}
           </div>
-          <div
-            data-scroll
-            data-scroll-sticky
-            data-scroll-target="#pin"
-            className="presentation-container-right"
-          >
+          <div data-scroll data-scroll-sticky data-scroll-target="#pin" className="presentation-container-right">
             {toggleHeader ? (
-              <img
-                className={`puff-in-center ${
-                  toggle ? "" : "presentation-img-dark"
-                }  `}
-                src={"/ryan.jpg"}
-                alt="ryan"
-              />
+              <img className={`puff-in-center ${toggle ? "" : "presentation-img-dark"}  `} src={"/ryan.jpg"} alt="ryan" />
             ) : (
-              <p
-                data-scroll
-                data-scroll-sticky
-                data-scroll-target="#pin"
-                className="puff-in-center "
-              >
-                Ancien militaire en reconversion, je cherche un stage non
-                rémunéré dans le but d'apprendre et de me perfectionner dans le
-                domaine du développement web .
+              <p data-scroll data-scroll-sticky data-scroll-target="#pin" className="puff-in-center ">
+                Développeur web full stack react & react native & express.
               </p>
             )}
           </div>
         </div>
 
-        <div
-          data-scroll
-          className={`section link ${toggle ? "link-light" : "link-dark"}`}
-        >
-          <h2
-            data-scroll
-            data-scroll-direction="horizontal"
-            data-scroll-speed="1"
-          >
+        <div data-scroll className={`section link ${toggle ? "link-light" : "link-dark"}`}>
+          <h2 data-scroll data-scroll-direction="horizontal" data-scroll-speed="1">
             Me contacter
           </h2>
           <div>
             <p>
-              <a
-                data-scroll
-                data-scroll-direction="horizontal"
-                data-scroll-speed="6"
-                target="blanc"
-                href="https://fr.linkedin.com/in/ryan-lollia-70393a213?trk=profile-badge"
-              >
+              <a data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" target="blanc" href="https://fr.linkedin.com/in/ryan-lollia-70393a213?trk=profile-badge">
                 <Icon icon={linkedinIcon} /> Linkedin
               </a>
             </p>
             <p>
-              <a
-                data-scroll
-                data-scroll-direction="horizontal"
-                data-scroll-speed="6"
-                target="blanc"
-                href="https://github.com/ryan77710"
-              >
+              <a data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" target="blanc" href="https://github.com/ryan77710">
                 <Icon icon={githubIcon} /> Lien Github
               </a>
             </p>
             <p>
-              <a
-                data-scroll
-                data-scroll-direction="horizontal"
-                data-scroll-speed="6"
-                target="_blanc"
-                href="mailto:ryanlollia77@gmail.com"
-              >
+              <a data-scroll data-scroll-direction="horizontal" data-scroll-speed="6" target="_blanc" href="mailto:ryanlollia77@gmail.com">
                 <Icon icon={googleGmail} /> ryanlollia77@gmail.com
               </a>
             </p>
           </div>
         </div>
-        <div
-          data-scroll
-          className={`section skills ${
-            toggle ? "skills-light" : "skills-dark"
-          }`}
-        >
+        <div data-scroll className={`section skills ${toggle ? "skills-light" : "skills-dark"}`}>
           <h2>Compétences</h2>
-          <div
-            data-scroll
-            data-scroll-direction="horizontal"
-            data-scroll-speed="-2"
-            className="skill"
-          >
-            <Technology
-              name="React"
-              toggle={toggle}
-              icon={reactIcon}
-              number={2}
-            />
-            <Technology
-              name="Html-Css"
-              toggle={toggle}
-              icon={fileTypeHtml}
-              icon2={fileTypeCss}
-              number={2}
-            />
-            <Technology
-              name="Javascript"
-              toggle={toggle}
-              icon={javascriptIcon}
-              number={2}
-            />
-            <Technology
-              name="React-native"
-              toggle={toggle}
-              icon={reactIcon}
-              number={2}
-            />
-            <Technology
-              name="Expo"
-              toggle={toggle}
-              icon={expoIcon}
-              number={1}
-            />
-            <Technology
-              name="Github"
-              toggle={toggle}
-              icon={githubIcon}
-              number={1}
-            />
-            <Technology
-              name="Jest"
-              toggle={toggle}
-              icon={fileTypeJest}
-              number={1}
-            />
-            <Technology
-              name="Express"
-              toggle={toggle}
-              icon={fileTypeNode}
-              number={2}
-            />
-            <Technology
-              name="Mongodb"
-              toggle={toggle}
-              icon={mongodbIcon}
-              number={2}
-            />
+          <div data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2" className="skill">
+            <Technology name="React" toggle={toggle} icon={reactIcon} number={2} />
+            <Technology name="Html-Css" toggle={toggle} icon={fileTypeHtml} icon2={fileTypeCss} number={2} />
+            <Technology name="Javascript" toggle={toggle} icon={javascriptIcon} number={2} />
+            <Technology name="React-native" toggle={toggle} icon={reactIcon} number={2} />
+            <Technology name="Expo" toggle={toggle} icon={expoIcon} number={1} />
+            <Technology name="Github" toggle={toggle} icon={githubIcon} number={1} />
+            <Technology name="Jest" toggle={toggle} icon={fileTypeJest} number={1} />
+            <Technology name="Express" toggle={toggle} icon={fileTypeNode} number={2} />
+            <Technology name="Mongodb" toggle={toggle} icon={mongodbIcon} number={2} />
           </div>
         </div>
-        <div
-          data-scroll
-          className={`section projects ${
-            toggle ? "project-light" : "project-dark"
-          } `}
-        >
+        <div data-scroll className={`section projects ${toggle ? "project-light" : "project-dark"} `}>
           <h2>Projects</h2>
           <div
             data-scroll
             data-scroll-direction="horizontal"
             data-scroll-speed="4"
-            className={`project-container ${
-              toggle ? "project-container-light" : "project-container-dark"
-            } `}
+            className={`project-container ${toggle ? "project-container-light" : "project-container-dark"} `}
           >
             <Project
               name="Vinted"
@@ -275,11 +133,7 @@ function App() {
               lien="https://ryan-marvel.netlify.app"
               text={projectText.marvel}
             />
-            <Project
-              name="Tellmemore(privé)"
-              src="https://res.cloudinary.com/ryan777/video/upload/v1623805655/portfolio/tellmemore_wttg9m.mov"
-              text={projectText.tellmemore}
-            />
+            <Project name="Tellmemore(privé)" src="https://res.cloudinary.com/ryan777/video/upload/v1623805655/portfolio/tellmemore_wttg9m.mov" text={projectText.tellmemore} />
 
             <Project
               name="Tripadvisor"
@@ -295,42 +149,23 @@ function App() {
             />
           </div>
         </div>
-        <div
-          data-scroll
-          className={`section others ${
-            toggle ? "others-light" : "others-dark"
-          }`}
-        >
+        <div data-scroll className={`section others ${toggle ? "others-light" : "others-dark"}`}>
           <h2>Autres</h2>
 
           <div className="picture-project-container">
             <div data-scroll data-scroll-speed="1">
-              <PictureProject
-                href="https://ryan-emoji.netlify.app"
-                src="emoji.png"
-                alt="ok"
-              />
+              <PictureProject href="https://ryan-emoji.netlify.app" src="emoji.png" alt="ok" />
               <p>Petit site pour trouver des émojies et les copier.</p>
             </div>
             <div data-scroll data-scroll-speed="2">
-              <PictureProject
-                href="https://ryan-to-do-list-react.netlify.app"
-                src="liste.png"
-                alt="ok"
-              />
-              <p>
-                Petit site To-Do liste permet d'ajouter une note, la rechercher,
-                et la supprimer.
-              </p>
+              <PictureProject href="https://ryan-to-do-list-react.netlify.app" src="liste.png" alt="ok" />
+              <p>Petit site To-Do liste permet d'ajouter une note, la rechercher, et la supprimer.</p>
             </div>
           </div>
         </div>
         <div data-scroll>
           <video className={`cv-video`} loop autoPlay="preload" muted>
-            <source
-              src="https://res.cloudinary.com/ryan777/video/upload/v1625190990/portfolio/Copie_de_lollia_ryan_cv_irswxl.mp4"
-              type="video/mp4"
-            />
+            <source src="https://res.cloudinary.com/ryan777/video/upload/v1682402416/portfolio/Copie_de_lollia_ryan_cv_static_plyubj.mp4" type="video/mp4" />
           </video>
         </div>
       </div>
